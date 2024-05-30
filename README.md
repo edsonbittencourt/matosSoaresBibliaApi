46d2c2721ee399c53e4a4d5e9c943b28bd45c274
koiyae <bruno.ryan.bs@gmail.com>
2024-05-30 20:22:45 -0300

    bible

---
+++ README.md
@@ -0,0 +1,18 @@
+# Flask Bible API
+
+Esta é uma simples API em Flask para acessar versículos da Bíblia armazenados em um arquivo CSV
+
+## Setup
+
+1. Clone este repositório:
+   ```bash
+   git clone https://github.com/devocionario/matosSoaresBibliaApi.git
+   cd matosSoaresBibliaApi
+
+2. Instale as dependências pip:
+   ```bash
+   pip install -r requirements.txt
+   
+3. Execute:
+   ```bash
+   flask run
---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, jsonify
+import csv
+
+app = Flask(__name__)
+
+def load_data():
+    with open('bible.csv', newline='', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        data = [row for row in reader]
+    return data
+
+@app.route('/biblia/<livro>/<int:capitulo>', methods=['GET'])
+@app.route('/biblia/<livro>/<int:capitulo>/', methods=['GET'])
+def get_versiculos(livro, capitulo):
+    data = load_data()
+    versiculos = []
+    for row in data:
+        if row['book'] == livro and int(row['chapter']) == capitulo:
+            versiculo = {
+                'book': row['book'],
+                'chapter': row['chapter'],
+                'verse': row['verse'],
+                'text': row['text']
+            }
+            versiculos.append(versiculo)
+    if versiculos:
+        versiculos = sorted(versiculos, key=lambda x: int(x['verse']))
+        return jsonify(versiculos)
+    else:
+        return jsonify({'message': 'Versículos não encontrados'}), 404
+
+@app.route('/biblia/<livro>/<int:capitulo>/<int:versiculo>', methods=['GET'])
+@app.route('/biblia/<livro>/<int:capitulo>/<int:versiculo>/', methods=['GET'])
+def get_versiculo(livro, capitulo, versiculo):
+    data = load_data()
+    for row in data:
+        if row['book'] == livro and int(row['chapter']) == capitulo and int(row['verse']) == versiculo:
+            versiculo = {
+                'book': row['book'],
+                'chapter': row['chapter'],
+                'verse': row['verse'],
+                'text': row['text']
+            }
+            return jsonify(versiculo)
+    return jsonify({'message': 'Versículo não encontrado'}), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
